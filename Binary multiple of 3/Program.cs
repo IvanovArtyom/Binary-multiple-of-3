5ca6e2a9b934d35462641efffe4318894675d47d
Артем Иванов <IvanovArtyom3000@yandex.ru>
2023-09-19 15:42:02 +0300

    Добавьте файлы проекта.

---
+++ Binary multiple of 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+public class BinaryRegex
+{
+    public static void Main()
+    {
+        // Test
+        Regex regex = MultipleOf3();
+        var t = regex.IsMatch("10101");
+        // ...should return true
+    }
+
+    public static Regex MultipleOf3()
+    {
+        return new Regex(@"\A(0|(1(01*(00)*0)*1)*)*\Z");
+    }
+}
